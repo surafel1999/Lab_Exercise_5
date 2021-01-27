7707d868a50f2fa231f362b412d441eaae97ca00
Surafel <se.surafel.kassahun@gmail.com>
2021-01-27 12:12:13 +0300

    Exercise done according to the comment

--- TaskManager/assets/js/bom.js
+++ TaskManager/assets/js/bom.js
@@ -18,6 +18,7 @@ Good Luck !!!
 
 
 // Define UI Variables  here 
+var required = document.getElementsByClassName("collection-item");
 
 
 
@@ -28,3 +29,34 @@ Good Luck !!!
 
 
 // Display the BOM Information on the innerHTML of the elements
+for (let i = 0; i < required.length; i++) {
+    if (required[i].textContent == "Href(Answer)") {
+        required[i].children[0].innerHTML = location.href;
+    } else if (required[i].textContent == "Protocol(Answer)") {
+        required[i].children[0].innerHTML = location.protocol;
+    } else if (required[i].textContent == "Host(Answer)") {
+        required[i].children[0].innerHTML = location.host;
+    } else if (required[i].textContent == "Port(Answer)") {
+        required[i].children[0].innerHTML = location.port;
+    } else if (required[i].textContent == "Hostname(Answer)") {
+        required[i].children[0].innerHTML = location.hostname;
+    } else if (required[i].textContent == "Appname(Answer)") {
+        required[i].children[0].innerHTML = navigator.appName;
+    } else if (required[i].textContent == "Platform(Answer)") {
+        required[i].children[0].innerHTML = navigator.platform;
+    } else if (required[i].textContent == "Language(Answer)") {
+        required[i].children[0].innerHTML = navigator.language;
+    } else if (required[i].textContent == "CookieEnabled(Answer)") {
+        required[i].children[0].innerHTML = navigator.cookieEnabled;
+    } else if (required[i].textContent == "Height(Answer)") {
+        required[i].children[0].innerHTML = screen.height;
+    } else if (required[i].textContent == "Width(Answer)") {
+        required[i].children[0].innerHTML = screen.width;
+    } else if (required[i].textContent == "PixelDepth(Answer)") {
+        required[i].children[0].innerHTML = screen.pixelDepth;
+    } else if (required[i].textContent == "Length(Answer)") {
+        required[i].children[0].innerHTML = history.length;
+    } else {
+        required[i].children[0].innerHTML = history.state;
+    }
+}
