b6a54f6b4f15e30ba9ac701d136633ca8aaacca2
Surafel <se.surafel.kassahun@gmail.com>
2021-02-01 21:17:44 +0300

    Code Refactored

--- TaskManager/assets/js/bom.js
+++ TaskManager/assets/js/bom.js
@@ -18,10 +18,12 @@ Good Luck !!!
 
 
 // Define UI Variables  here 
-var required = document.getElementsByClassName("collection-item");
+var required = document.querySelectorAll(".badge");
 
 
-
+var value = [location.href, location.protocol, location.host, location.port, location.hostname, navigator.appName, 
+    navigator.appVersion, navigator.platform, navigator.language, navigator.cookieEnabled, screen.height, screen.width,
+    screen.pixelDepth, history.length, history.state];
 
 
 
@@ -30,33 +32,5 @@ var required = document.getElementsByClassName("collection-item");
 
 // Display the BOM Information on the innerHTML of the elements
 for (let i = 0; i < required.length; i++) {
-    if (required[i].textContent == "Href(Answer)") {
-        required[i].children[0].innerHTML = location.href;
-    } else if (required[i].textContent == "Protocol(Answer)") {
-        required[i].children[0].innerHTML = location.protocol;
-    } else if (required[i].textContent == "Host(Answer)") {
-        required[i].children[0].innerHTML = location.host;
-    } else if (required[i].textContent == "Port(Answer)") {
-        required[i].children[0].innerHTML = location.port;
-    } else if (required[i].textContent == "Hostname(Answer)") {
-        required[i].children[0].innerHTML = location.hostname;
-    } else if (required[i].textContent == "Appname(Answer)") {
-        required[i].children[0].innerHTML = navigator.appName;
-    } else if (required[i].textContent == "Platform(Answer)") {
-        required[i].children[0].innerHTML = navigator.platform;
-    } else if (required[i].textContent == "Language(Answer)") {
-        required[i].children[0].innerHTML = navigator.language;
-    } else if (required[i].textContent == "CookieEnabled(Answer)") {
-        required[i].children[0].innerHTML = navigator.cookieEnabled;
-    } else if (required[i].textContent == "Height(Answer)") {
-        required[i].children[0].innerHTML = screen.height;
-    } else if (required[i].textContent == "Width(Answer)") {
-        required[i].children[0].innerHTML = screen.width;
-    } else if (required[i].textContent == "PixelDepth(Answer)") {
-        required[i].children[0].innerHTML = screen.pixelDepth;
-    } else if (required[i].textContent == "Length(Answer)") {
-        required[i].children[0].innerHTML = history.length;
-    } else {
-        required[i].children[0].innerHTML = history.state;
-    }
+    required[i].innerHTML = value[i];
 }
